34c51e5d11cca9968cd61ae0647413164fab68ca
Katharine Zarbock <kjzarbock@gmail.com>
2023-07-12 17:37:34 -0500

    7/12/23 1737, setup

---
+++ views/order_requests.py
@@ -0,0 +1,54 @@
+ORDERS = [
+    {
+        "metalId": 4,
+        "sizeId": 3,
+        "styleId": 2,
+        "id": 1
+    },
+    {
+        "metalId": 5,
+        "sizeId": 5,
+        "styleId": 3,
+        "id": 2
+    },
+    {
+        "metalId": 5,
+        "sizeId": 3,
+        "styleId": 2,
+        "id": 3
+    },
+    {
+        "metalId": 4,
+        "sizeId": 3,
+        "styleId": 3,
+        "id": 4
+    },
+    {
+        "metalId": 4,
+        "sizeId": 3,
+        "styleId": 1,
+        "id": 5
+    },
+    {
+        "metalId": 3,
+        "sizeId": 4,
+        "styleId": 2,
+        "id": 6
+    },
+    {
+        "metalId": 2,
+        "sizeId": 3,
+        "styleId": 3,
+        "id": 7
+    },
+    {
+        "metalId": 3,
+        "sizeId": 4,
+        "styleId": 1,
+        "id": 8
+    }
+]
+
+
+def get_all_orders():
+    return ORDERS
